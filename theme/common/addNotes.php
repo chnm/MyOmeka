eb2b6e0bc15872a60a96ce48f5fdb020e3098500
Jon Lesser <jonlesser@gmail.com>
2008-06-26 22:59:44 +0000

    Fixed #422 Add notes text area now shows without having to click Added tinyMCE to text area

--- theme/common/addNotes.php
+++ theme/common/addNotes.php
@@ -1,13 +1,11 @@
-<?php echo js('addNotes'); ?>
+<?php
+    echo js('tiny_mce/tiny_mce'); 
+    echo js('addNotes'); 
+?>
+
 <div id="myomeka-add-notes">
 	<a class="dashboard-link" href="<?php echo uri('myomeka'); ?>">Go to My Dashboard</a>
    <h2>Your Notes</h2>
-    <div id="myomeka-display-note">
-        <?php if($note): ?>
-        <div id="myomeka-note"><?php echo nls2p($note->note);?></div>
-        <?php endif; ?>
-        <div><a id="myomeka-edit-link" href="#">Edit your notes</a></div>
-    </div>
     <div id="myomeka-edit-note">
         <form action="<?php print uri("note/submit"); ?>" id="myomeka-note-form" method="post" accept-charset="utf-8">    
             <div>
@@ -15,8 +13,7 @@
                     <label>Enter your notes:</label>
                     <textarea name="note" rows="5" cols="60"><?php echo $note->note; ?></textarea>
                 </div>
-                <input type="submit" id="myomeka-submit-note" value="Save Notes"/> 
-                or <a href="#" id="myomeka-cancel-edit">Cancel</a>
+                <input type="submit" id="myomeka-submit-note" value="Save Notes"/>
                 <input type="hidden" name="item_id" value="<?php print $item->id;?>"/>
             </div>
         </form>
--- theme/javascripts/addNotes.js
+++ theme/javascripts/addNotes.js
@@ -1,19 +1,28 @@
 Event.observe(window, 'load', function(){
-    
-    // Hide the form
-    $$("#myomeka-edit-note").invoke("hide");
-    
-    // Set up edit link
-    $$("#myomeka-edit-link").invoke("observe", "click", function(e){
-        $$("#myomeka-display-note").invoke("hide");
-        $$("#myomeka-edit-note").invoke("show");
-        Event.stop(e);
-    });
-    
-    // Set up cancel link
-    $$("#myomeka-cancel-edit").invoke("observe", "click", function(e){
-        $$("#myomeka-display-note").invoke("show");
-        $$("#myomeka-edit-note").invoke("hide");
-        Event.stop(e);
-    });
+
+    tinyMCE.init({
+    	mode : "textareas",
+    	theme: "advanced",
+    	theme_advanced_toolbar_location : "top",
+    	theme_advanced_buttons1 : "bold,italic,underline,justifyleft,justifycenter,justifyright,bullist,numlist,link,formatselect",
+		theme_advanced_buttons2 : "",
+		theme_advanced_buttons3 : "",
+		theme_advanced_toolbar_align : "left"
+    });    
+    // // Hide the form
+    // $$("#myomeka-edit-note").invoke("hide");
+    // 
+    // // Set up edit link
+    // $$("#myomeka-edit-link").invoke("observe", "click", function(e){
+    //     $$("#myomeka-display-note").invoke("hide");
+    //     $$("#myomeka-edit-note").invoke("show");
+    //     Event.stop(e);
+    // });
+    // 
+    // // Set up cancel link
+    // $$("#myomeka-cancel-edit").invoke("observe", "click", function(e){
+    //     $$("#myomeka-display-note").invoke("show");
+    //     $$("#myomeka-edit-note").invoke("hide");
+    //     Event.stop(e);
+    // });
 });
