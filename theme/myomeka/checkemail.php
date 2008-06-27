96fdc0b62e0073763a777a4274d7583f4cff83ae
Will Riley <wanderingwill@gmail.com>
2008-06-27 18:54:39 +0000

    After user registers, they now go to a page which tells them to check their email.

---
+++ theme/myomeka/checkemail.php
@@ -0,0 +1,6 @@
+<?php head(); ?>
+<div id="myomeka-primary">
+	<h2>Register</h2>
+	<p>Your account has been successfully created.  An email has been sent to you to complete your registration.</p>
+</div>
+<?php foot(); ?>
