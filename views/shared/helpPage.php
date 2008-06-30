0b6b0c4c2cdfe72db6259098b72287b328ba7ff3
Jeremy Boggs <jeremy@clioweb.org>
2008-06-30 23:48:19 +0000

    Fixes #416 Create static page with help text.

--- views/shared/helpPage.php
+++ views/shared/helpPage.php
@@ -3,8 +3,11 @@
 <div id="myomeka-primary">
 	<div id="help_page">
 		<h2>Help Page</h2>
-		<p></p>
-	</div>
+        <h3>Your Notes</h3>
+        <p>When logged in, you are able to add notes to any of the items on the website. Click on the &#8220;Edit Your Notes&#8221; link in the &#8220;Your Notes&#8221; section to begin a new notation or edit an existing one. These notes will be stored and will appear on the item pages any time you are logged in. A list of the items you have annotated will appear on your dashboard. These notes will also be available for use if you choose to make a poster with your items.</p>
+
+        <h3>Your Tags</h3>
+        <p>When logged in, you are also able to tag items on the website. Tags are like keywords or labels, allowing you to categorize and group items. Simply type a tag into the &#8220;Add a tag&#8221; field in &#8220;Your Notes,&#8221; and click add. Items can have multiple tags. A list of all the tags you use will appear on your dashboard. Clicking on any of these tags will list all of the items to which you have given this tag. For example, clicking on &#8220;island&#8221; will show you a list of all the items you have tagged &#8220;island.&#8221;</p>	</div>
 </div>
 
 <?php foot(); ?>
