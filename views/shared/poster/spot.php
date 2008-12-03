d627aed4fa1448e931920c516174bd630d8d6d2d
Jeremy Boggs <jeremy@clioweb.org>
2008-12-03 21:30:49 +0000

    Fixes bug that prevented items from saving to poster.

--- views/shared/poster/spot.php
+++ views/shared/poster/spot.php
@@ -1,8 +1,7 @@
 <?php set_current_item($posterItem); ?>
 <div class="myomeka-poster-spot">
-	
-	<h3 class="myomeka-item-title"><?php echo item('Dublin Core', 'Title'); ?></h3>	
-	
+
+		        <h3 class="myomeka-item-title"><?php echo item('Dublin Core', 'Title'); ?></h3>
     <div class="myomeka-controls">
         <a href="#" class="myomeka-move-top myomeka-poster-control">
             <img src="<?php echo img('arrow_up_up.png'); ?>"  title="Move to the top" alt="Move to the top"/></a>
@@ -11,21 +10,22 @@
         <a href="#" class="myomeka-move-down myomeka-poster-control">
             <img src="<?php echo img('arrow_down.png'); ?>"  title="Move down" alt="Move down"/></a>
         <a href="#" class="myomeka-move-bottom myomeka-poster-control">
-            <img src="<?php echo img('arrow_down_down.png'); ?>"  title="Move to the bottom" alt="Move to the bottom"/></a>
+            <img src="<?php echo img('arrow_down_down.png'); ?>"  title="Move to the bottom" alt="Move to the bottom" /></a>
+
     </div>
 
     <div class="myomeka-item-thumbnail">
         <?php echo poster_icon_html(); ?>
     </div>
 
-    <div class="myomeka-item-annotation">
+    <div class="myomeka-annotation">
         <?php echo __v()->formTextarea('annotation-' . $posterItem->ordernum, $noteText,
             array(  'id'=>'myomeka-annotation-' . mt_rand(0, 999999999),
                     'rows'=>'6',
                     'cols'=>'10')); ?>
     </div>
     <?php if ($noteText): ?>
-        <div class="myomeka-item-notes">
+        <div class="myomeka-notes">
             <h3>My Notes</h3>
             <?php echo nls2p(htmlspecialchars($noteText)); ?>
         </div>
@@ -35,6 +35,5 @@
 		<a href="#" class="myomeka-delete myomeka-poster-control">
             <img src="<?php echo img('delete.gif'); ?>" title="Remove this item" alt="Remove this item"/>Delete</a>
 	</div>
-	
     <input type="hidden" name="itemID-<?php echo $posterItem->ordernum; ?>" value="<?php echo $posterItem->id; ?>" class="myomeka-hidden-item-id" />
 </div>
